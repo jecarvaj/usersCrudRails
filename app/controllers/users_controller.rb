33def8be08b44525cdd0fea515d7cc4b2c6a3797
Jean Carvajal <jecarvaj@alumnos.ubiobio.cl>
2017-12-07 16:21:26 -0300

    Agrego funcionalidad para agregar redes sociales

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -15,31 +15,25 @@ class UsersController < ApplicationController
   # GET /users/new
   def new
     @user = User.new
-
   end
 
   # GET /users/1/edit
   def edit
-
   end
 
   # POST /users
   # POST /users.json
   def create
     @user = User.new(user_params)
-
-
-
     respond_to do |format|
       if @user.save
-        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.html { redirect_to users_path, notice: 'Usuario creado correctamente.' }
         format.json { render :show, status: :created, location: @user }
       else
         format.html { render :new }
         format.json { render json: @user.errors, status: :unprocessable_entity }
       end
     end
-
   end
 
   # PATCH/PUT /users/1
@@ -47,7 +41,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
-        format.html { redirect_to @user, notice: 'User was successfully updated.' }
+        format.html { redirect_to @user, notice: 'Usuario actualizado correctamente.' }
         format.json { render :show, status: :ok, location: @user }
       else
         format.html { render :edit }
@@ -61,21 +55,20 @@ class UsersController < ApplicationController
   def destroy
     @user.destroy
     respond_to do |format|
-      format.html { redirect_to users_url, notice: 'User was successfully destroyed.' }
+      format.html { redirect_to users_url, notice: 'Usuario eliminado.' }
       format.json { head :no_content }
     end
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
+  # Use callbacks to share common setup or constraints between actions.
+  def set_user
+    @user = User.find(params[:id])
+  end
 
-    end
-
-    # Never trust parameters from the scary internet, only allow the white list through.
-    def user_params
-      params.require(:user).permit(:rut, :first_name, :last_name, :mail, :birthday, :password, :password_confirmation, :image,
-                                   profiles_attributes:[:id, :name, :url, :_destroy])
-    end
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def user_params
+    params.require(:user).permit(:rut, :first_name, :last_name, :mail, :birthday, :password, :password_confirmation, :image,
+                                 profiles_attributes:[:id, :name, :url, :_destroy])
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ApplicationRecord
   has_secure_password
+  has_attached_file :image, styles: { medium: '200x200', thumb: '100x100#' }, default_url:"http://www.fastpoke.com/wp-content/themes/wpjobus/images/User_default.png"
+  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
+  #Relacion para un user tiene muchos profiles
   has_many :profiles, inverse_of: :user
   accepts_nested_attributes_for :profiles, reject_if: :all_blank, allow_destroy: true
 
+  #Validaciones
   before_save :capitalize_names
   validates :rut, :first_name, :last_name, :mail, presence: true
   validate :valid_date
   validates_format_of :mail, :with =>
       /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\Z/i
-
-  has_attached_file :image, styles: { medium: '200x200', thumb: '100x100#' }, default_url:"http://www.fastpoke.com/wp-content/themes/wpjobus/images/User_default.png"
-  validates_attachment_content_type :image, content_type: /\Aimage\/.*\z/
 
   def capitalize_names
     self.first_name=self.first_name.titleize
@@ -19,6 +20,6 @@ class User < ApplicationRecord
   end
 
   def valid_date
-    errors.add(:birthday, "Nacimiento en el futuro?") unless birthday <= Date.today
+    errors.add(:birthday, "Nacimiento futuro?") unless birthday <= Date.today
   end
 end
